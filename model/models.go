db494143a84eeb90977fc7da2db42fcdb917f410
CheyuWu <cheyu1220@gmail.com>
2022-06-11 16:44:57 +0800

    chore: restart again

--- model/models.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "github.com/google/uuid"
-
-type Company struct {
-	ID          uuid.UUID `json:"id"`
-	Name        string    `json:"name"`
-	StockSymbol string    `json:"stockSymbol"`
-}

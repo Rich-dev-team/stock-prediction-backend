da3e17039b966d10d59cbadc0b4c0f12b86a970a
CheyuWu <cheyu1220@gmail.com>
2022-11-06 16:25:52 +0800

    feat: mock db (not done)

--- README.md
+++ README.md
@@ -14,3 +14,6 @@
 
 ## Debug postgres lock
 > https://wiki.postgresql.org/wiki/Lock_Monitoring
+
+## Add go env to path
+> `export PATH=$PATH:~/go/bin`
--- api/service.go
+++ api/service.go
@@ -8,12 +8,12 @@ import (
 
 // Server serves HTTP requests for our stock
 type Server struct {
-	store  *db.Store
+	store  db.Store
 	router *gin.Engine
 }
 
 // New server creates a new HTTP Server and setup routing
-func NewServer(store *db.Store) *Server {
+func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 
 	router := gin.Default()
